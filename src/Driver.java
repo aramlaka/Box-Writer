60004ac107a789c68fe826d9f0800fbc07a6f7e8
aramlaka <Akhil.ramlakan@gmail.com>
2016-01-14 11:49:56 -0500

    Added copy to clipboard function

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,11 @@
 /**
  * @author Akhil Ramlakan
  *
+ * Creates 3D text boxes.
  */
 
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
 import java.util.*;
 
 public class Driver
@@ -11,11 +14,22 @@ public class Driver
     {
         Scanner input = new Scanner(System.in);
         BoxWriter box;
+        char yesno;
 
-        System.out.print("\nEnter in some meme text (min 3 characters):\n");
+        System.out.print("\nEnter in some meme text (min 3 characters): ");
         box = new BoxWriter(input.nextLine());
 
+        System.out.print("Copy to clipboard? (Y/N): ");
+        yesno = input.next().toUpperCase().charAt(0);
+
         box.boxify();
+
+        if(yesno == 'Y')
+        {
+            StringSelection stringSelection = new StringSelection(box.toString());
+            Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clpbrd.setContents(stringSelection, null);
+        }
 
         System.out.print(box);
     }
